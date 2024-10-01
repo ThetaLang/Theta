cde9a250c01df53e0346b4863c1bb0e71ac0669a
Miguel Cárdenas <miguelecsx@gmail.com>
2024-10-01 11:07:10 -0500

    chore: #38 reset project version to 0.1.0
    - resetting the project version to better reflect the current level of completeness
    
    - closes #38

--- version.h
+++ version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 const unsigned int VERSION_MAJOR = 0;
-const unsigned int VERSION_MINOR = 11;
-const unsigned int VERSION_PATCH = 5;
+const unsigned int VERSION_MINOR = 1;
+const unsigned int VERSION_PATCH = 0;
 
 #endif
